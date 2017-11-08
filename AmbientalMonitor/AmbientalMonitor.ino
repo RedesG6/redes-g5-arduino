51f178e19166efd1fb6519d6464c2ae0d799f3f1
Wilson Farias <wilsonsf1@gmail.com>
2017-11-08 10:38:49 -0300

    Arduino: reparo no log da hora e calculo do tempo de atraso
    - Fix: tempo de atraso para leitura - considerando atraso da leitura da temperatura (tempo mínimo de 1s)
    - Refatoração do registro de log da hora e calculo do tempo de delay

--- AmbientalMonitor/AmbientalMonitor.ino
+++ AmbientalMonitor/AmbientalMonitor.ino
@@ -1,6 +1,6 @@
 /*
   Ambiental Monitor using luminosity and temperature sensors.
-  
+
   TO DO:
   - Communicating with a rPI through serial.
 
@@ -9,7 +9,7 @@
 */
 #define referencePotencial 5.0 // Power source to LDR
 #define miliToVolts 100.0   // convert mV to V (Volts)
-#define timeInterval 1.0/30.0 // Time interval in minutes
+#define timeInterval 1.0/30.0 // Time interval in minutes - minimum 1.0/60.0, due reading time
 #define millisToMinute 1000*60
 
 int ledPinBuiltin = LED_BUILTIN;
@@ -19,7 +19,7 @@ int lmPin = A0;
 
 void setup() {
   Serial.begin(115200);
-  Serial.println("Hora inicial: 22:10");
+  Serial.println("Hora inicial: 10:30");
   pinMode(ledPinBuiltin, OUTPUT);
   pinMode(ledPin, OUTPUT);
 }
@@ -30,29 +30,36 @@ void loop() {
   int luminosity = 0;
   int celsiusTemp = 0;
   int fahrenheitTemp = 0;
-  int samples[8];
 
-  for (int i = 0; i <= 7; i++) {
-    samples[i] = ( referencePotencial * analogRead(lmPin) * miliToVolts) / 1024.0;
-    //A cada leitura, incrementa o valor da variavel celsiusTemp
-    celsiusTemp = celsiusTemp + samples[i];
-    delay(100);
-  }
-  celsiusTemp = celsiusTemp / 8.0;
-  fahrenheitTemp = (celsiusTemp * 9) / 5 + 32;
-  
-  Serial.print("T+");
-  Serial.print(millis()/1000/60);
-  Serial.println("(min)");
-  
+  logReadingTime();
+
+  temperatureReading(celsiusTemp, fahrenheitTemp);
+  temperatureLogging(celsiusTemp, fahrenheitTemp);
+
   luminosityReading(ldrReading, luminosity);
   luminosityLogging(ldrReading, luminosity);
 
-  temperatureLogging(celsiusTemp, fahrenheitTemp);
-  delay(timeInterval * millisToMinute);
+  delay(calculateDelayTime());
 }
 
-void luminosityReading(int& state, int& luminosity){
+void logReadingTime() {
+  Serial.print("T+ ");
+
+  long segundos = millis() / 1000;
+  long minutos = segundos / 60;
+  segundos -= minutos * 60;
+  long horas = minutos / 60;
+  minutos -= horas * 60;
+
+  Serial.print(horas);
+  Serial.print(":");
+  Serial.print(minutos);
+  Serial.print(":");
+  Serial.print(segundos);
+  Serial.println(" HH:mm:ss");
+}
+
+void luminosityReading(int& state, int& luminosity) {
   state = analogRead(ldrPin);
   Serial.print("Valor lido do LDR: ");
   Serial.println(state);
@@ -63,7 +70,7 @@ void luminosityReading(int& state, int& luminosity){
   Serial.println();
 }
 
-void luminosityLogging(int& state, int& luminosity){
+void luminosityLogging(int& state, int& luminosity) {
   analogWrite(ledPin, luminosity);
 
   if (state > 800) {
@@ -76,6 +83,19 @@ void luminosityLogging(int& state, int& luminosity){
   }
 }
 
+void temperatureReading(int& celsius, int& farenheit) {
+  int samples[8];
+
+  for (int i = 0; i <= 7; i++) {
+    samples[i] = ( referencePotencial * analogRead(lmPin) * miliToVolts) / 1024.0;
+    //A cada leitura, incrementa o valor da variavel celsiusTemp
+    celsius = celsius + samples[i];
+    delay(100);
+  }
+  celsius = celsius / 8.0;
+  farenheit = (celsius * 9) / 5 + 32;
+}
+
 void temperatureLogging(int& celsius, int& farenheit) {
   Serial.print("Temperatura: ");
   Serial.print(celsius, DEC);
@@ -83,5 +103,9 @@ void temperatureLogging(int& celsius, int& farenheit) {
   Serial.print(farenheit, DEC);
   Serial.println(" \u00B0F.");
   Serial.println();
+}
+
+long calculateDelayTime() {
+  return (long) (timeInterval * millisToMinute) - millis() % (long)(timeInterval * millisToMinute);
 }
 
